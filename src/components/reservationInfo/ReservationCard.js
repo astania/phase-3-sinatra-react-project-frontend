e216d67a8dc80904ab81b0c0d3436f0252b94cf3
+ <+>
2022-06-30 12:11:04 -0400

    update reservation card

--- src/components/reservationInfo/ReservationCard.js
+++ src/components/reservationInfo/ReservationCard.js
@@ -1,8 +1,15 @@
 
 
-const ReservationCard = () => {
+const ReservationCard = ({reservation}) => {
   return (
-    <div>ReservationCard</div>
+    <div>
+      <img src={reservation.restaurant.image_url} width="100" height="100"/>
+      <h4>{reservation.restaurant.restaurant_name}</h4>
+      <p>{reservation.date}</p>
+      <p>{reservation.time}</p>
+      <p>Number of guests: {reservation.number_of_guests}</p>
+      <button>cancel reservation</button>
+    </div>
   )
 }
 
