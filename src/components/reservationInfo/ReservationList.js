8ad139d99e51e99e6c3b503f8c2201284a7fe59e
+ <+>
2022-07-01 13:40:56 -0400

    update my reservation page for users with no reservations

--- src/components/reservationInfo/ReservationList.js
+++ src/components/reservationInfo/ReservationList.js
@@ -3,13 +3,21 @@ import ReservationCard from './ReservationCard'
 const ReservationList = ({ loggedInGuest, onDeleteReservation }) => {
   let reservations = loggedInGuest.reservations
 
-  return (
-    <div>
-      <h3>{loggedInGuest.name}'s Reservations</h3>
-      {reservations.map(reservation => <ReservationCard reservation={reservation} key={reservation.id} onDeleteReservation={onDeleteReservation}/>)}
-    </div>
+  if (reservations.length > 0) {
+    return (
+      <div>
+        <h3>{loggedInGuest.name}'s Reservations</h3>
+        {reservations.map(reservation => <ReservationCard reservation={reservation} key={reservation.id} onDeleteReservation={onDeleteReservation}/>)}
+      </div>
+  
+    )
+  } else {
+    return (
+      <h3><em>You have no reservations</em></h3>
+    )
+  }
 
-  )
+  
 }
 
 export default ReservationList
